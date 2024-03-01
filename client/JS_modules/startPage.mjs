678fbcf5cf966013e3ddae2cfefbec2ed34ba284
Amanda Hansson <amanda9307@gmail.com>
2024-03-01 14:06:42 +0100

    :sparkles: feat [index.html, main.js, loginPage.mjs, startPAge.mjs, app.js] edited old files for compability with new backend endpoint for generating chatrooms. Created array to save shatrooms in backend, connected loginPage and startPage to one another

--- client/JS_modules/startPage.mjs
+++ client/JS_modules/startPage.mjs
@@ -2,9 +2,8 @@
 // import createChatroom from createChatroom
 // import enterChatroom from enterChatroom
 import { io } from "socket.io-client";
-
-const socket = io("https://squid-app-cg7rw.ondigitalocean.app/");
-let HTMLContainer = document.getElementById("HTMLContainer"); // Where we render all HTML in index.html
+const socket = io("http://localhost:3000"); //https://squid-app-cg7rw.ondigitalocean.app/
+let mainContainer = document.getElementById("main"); // Where we render all HTML in index.html
 
 /**
  * startPage
@@ -19,45 +18,40 @@ let HTMLContainer = document.getElementById("HTMLContainer"); // Where we render
  * Appends all content in nested containers an finally in the "main" container in index.html
  */
 
-export default function startPage(userId) {
+export default function startPage() {
+    mainContainer.innerHTML = "";
     let startPageContainer = document.createElement("div");
     startPageContainer.classList.add("start-page-container"); // For CSS
     let startPageBtnContainer = document.createElement("div");
     startPageBtnContainer.classList.add("start-page-btn-container"); // For CSS
     const logOutBtn = document.createElement("button");
+    logOutBtn.textContent = "Logga ut";
     const createChatroomBtn = document.createElement("button");
+    createChatroomBtn.textContent = "Skapa nytt chattrum";
     const chatrooms = document.createElement("div");
 
     // logOutBtn.addEventListener("click", logOut());
     // createChatroomBtn.addEventListener("click", createChatroom())
-
-    socket.on("getChatrooms", (chatroomArray) => {
-        if (chatroomArray.chatroom) { // If there are any chatrooms (where?)
-
-            chatroomArray.forEach(chatroomName => { // Loop trough all existing chatrooms
-                let li = document.createElement("li");
-                li.innerText = chatroomName.chatroom;
-                let enterChatroomBtn = document.createElement("button");
-                enterChatroomBtn.textContent = "Enter Chatroom";
-                li.appendChild(enterChatroomBtn);
-                chatrooms.appendChild(li);
-
-                // enterChatroomBtn.addEventListener("click", () => { // Call on function for entering specific chatroom (provide id or name of room)
-                //     enterChatroom(chatroomName.chatroom);
-                // })
-            });
-
+    socket.emit("getChatrooms");
+    socket.on("getChatrooms", (savedChatroom) => {
+        if (savedChatroom) { // If there are any chatrooms (where?)
+            let li = document.createElement("li");
+            li.innerText = savedChatroom.chatroomName;
+            let enterChatroomBtn = document.createElement("button");
+            enterChatroomBtn.textContent = "Enter Chatroom";
+            li.appendChild(enterChatroomBtn);
+            chatrooms.appendChild(li);
+            // enterChatroomBtn.addEventListener("click", () => { // Call on function for entering specific chatroom (provide id or name of room)
+            //     enterChatroom(chatroomName.chatroom);
+            // })
         } else {
             console.log("No chatrooms available");
         }
     })
 
-
-
-    HTMLContainer = "";
     startPageBtnContainer.append(logOutBtn, createChatroomBtn); // Puts buttons in a separate container
     startPageContainer.append(startPageBtnContainer, chatrooms); // Puts the button container and all the chatrooms in container for all content
-    HTMLContainer.appendChild(startPageContainer); // Puts all content in the index.html-container
+    mainContainer.appendChild(startPageContainer); // Puts all content in the index.html-container
 
 
 }
