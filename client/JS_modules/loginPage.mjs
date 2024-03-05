554bda437e072dabb012086b57bf0888297f2860
Amanda Hansson <amanda9307@gmail.com>
2024-03-05 10:33:48 +0100

    :lipstick: styling [index.html, loginPage.mjs]: edited loginpage as per new wireframe.

--- client/JS_modules/loginPage.mjs
+++ client/JS_modules/loginPage.mjs
@@ -4,23 +4,33 @@ import startPage from "./startPage.mjs";
 let mainContainer = document.getElementById("main");
 
 export default function loginPage() {
-
   // Genererar html för inloggning 
   mainContainer.innerHTML = `
-  <h1>Välkommen till ChatRooms - gridSock6!</h1>
+  <p>Welcome to</p>
+  <h2>WAR OF THE PALETTES</h2>
   <input
     type="text"
     id="userNameInput"
-    placeholder="Skriv in ditt namn här..."
+    placeholder="Enter your player name"
   />
-  <button id="saveUserNameBtn">Logga In</button>`;
+  <button id="saveUserNameBtn" style =
+  .material-symbols-outlined {
+    font-variation-settings:
+    'FILL' 0,
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 24
+  }
+  ><span class="material-symbols-outlined">
+  arrow_forward
+  </span></button>`;
 
   // Kopplar till ovan genererad html
   let userNameInput = document.getElementById('userNameInput');
   let saveUserNameBtn = document.getElementById('saveUserNameBtn');
 
   // Error meddelande som skrivs ut om man inte valt ett användarnamn men försöker trycka på logga in knappen
-  let errorMsg = "Du måste välja ett namn!"
+  let errorMsg = "You need to pick a name"
 
   saveUserNameBtn.addEventListener("click", () => {
     // If sats för att se om användarnamn är ok eller ej. 
