27aff03883de069247a061f6dc09b199021eaf00
Michael Inma <112844696+Divorso@users.noreply.github.com>
2024-03-05 18:33:50 +0700

    Update main.js
    Borta

--- client/main.js
+++ client/main.js
@@ -6,16 +6,3 @@ const socket = io('http://localhost:3000'); //https://squid-app-cg7rw.ondigitalo
 // Kör login page funktionen vid start
 loginPage();
 
-document.addEventListener('DOMContentLoaded', function () {
-  const container = document.getElementById('grid-container');
-  let gridSize = 15; // Size of the grid (15x15)
-  for (let row = 0; row < gridSize; row++) {
-    for (let col = 0; col < gridSize; col++) {
-      let cell = document.createElement('div');
-      cell.classList.add('grid-item');
-      // Assign a unique ID to each cell based on its row and column
-      cell.id = `cell-${row}-${col}`;
-      container.appendChild(cell);
-    }
-  }
-});
