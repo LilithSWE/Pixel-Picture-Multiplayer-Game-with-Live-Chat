0eba09617ae3c876add7557c17d2966422bdf736
Amanda Hansson <amanda9307@gmail.com>
2024-03-06 15:12:15 +0100

    :sparkles: feat [loads of JS files]: Started the gamePage generator, split into multiple files. Transferred the chat function from the previous assignment to the new build. Tested and functioning. Added option to style classlists to differentiate the different messages depending om you or the team sent the message. TBQ.

--- client/JS_modules/gridGenerator.mjs
+++ client/JS_modules/gridGenerator.mjs
@@ -1,21 +1,25 @@
 export default function gridGenerator() {
-const container = document.getElementById('main');
-//spelarens färg
-let colorPlayer = 'red';
-//Grid-Size ändras här
-let gridRow = 15;
-let gridColumn = 15;
-for (let row = 0; row < gridRow; row++) {
-  for (let col = 0; col < gridColumn; col++) {
-    let cell = document.createElement('div');
-    cell.classList.add('grid-item');
-    cell.id = `Row-${row}-Column-${col}`;
+  const container = document.getElementById('main');
+  //spelarens färg
+  let colorPlayer = 'red';
 
-    cell.addEventListener('click', () => {
-      if (cell.style.backgroundColor != colorPlayer) {
-        cell.style.backgroundColor = colorPlayer;
-      }
-    });
-    container.appendChild(cell);
+  //Grid-Size ändras här
+  let gridRow = 15;
+  let gridColumn = 15;
+
+  for (let row = 0; row < gridRow; row++) {
+    for (let col = 0; col < gridColumn; col++) {
+      let cell = document.createElement('div');
+      cell.classList.add('grid-item');
+      cell.id = `Row-${row}-Column-${col}`;
+
+      cell.addEventListener('click', () => {
+        if (cell.style.backgroundColor != colorPlayer) {
+          cell.style.backgroundColor = colorPlayer;
+        }
+      });
+      container.appendChild(cell);
+    }
   }
-}};
+};
+
---
+++ client/JS_modules/timerAndBtnsGamePage.mjs
@@ -0,0 +1,3 @@
+export default function generateTimerAndBtnGamePage() {
+  console.log("här kommer timer and btns genereras");
+};
