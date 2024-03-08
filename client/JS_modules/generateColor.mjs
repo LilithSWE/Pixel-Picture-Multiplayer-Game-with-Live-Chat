6436d3c6fa9a0d5f5a5babe1e05f5bccaa0bfb38
Augustine Al-Zebary <146852586+Ogen1998@users.noreply.github.com>
2024-03-08 12:23:49 +0100

    Created generateColor :sparkles:

---
+++ client/JS_modules/generateColor.mjs
@@ -0,0 +1,23 @@
+//Knapp klickad
+//Random bild har hämtats från JSON
+//Hämta colors från den bilden
+//Dela ut färgena till username som finns i localstorage
+//Spara färgen i localstorage
+
+const colors = ["red", "green", "blue", "yellow"]; //endast för TESTNING, kommer att raderas
+
+const testBTN = document.getElementById('testBTN');
+
+testBTN.addEventListener('click', generateColor);
+
+export default function generateColor() {
+    var usernames = ['user1', 'user2', 'user3', 'user4']; //testning
+
+    for (var i = 0; i < usernames.length; i++) {
+        var username = usernames[i];
+        var color = colors[i];
+
+        // Spara färgen i localstorage för varje användarnamn
+        localStorage.setItem(username, color);
+    }
+}
--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 // import loginPage from './JS_modules/loginPage.mjs';
 import landingPage from './JS_modules/landingPage.mjs';
+import generateColor from './JS_modules/generateColor.mjs';
 
 import { io } from 'socket.io-client';
 const socket = io('http://localhost:3000'); //https://squid-app-cg7rw.ondigitalocean.app/
