f2d35ea3c3606d91c60733119af14b1f36974382
Augustine Al-Zebary <146852586+Ogen1998@users.noreply.github.com>
2024-03-11 09:39:03 +0100

    :lipstick: added fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,10 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'inter': ['Inter', 'sans-serif'],
+      'slab': ['"Josefin Slab"', 'serif']
+    },
     extend: {},
   },
   plugins: [],
