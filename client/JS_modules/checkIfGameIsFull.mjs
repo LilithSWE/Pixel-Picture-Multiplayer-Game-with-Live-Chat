8f789c55584afc4ed86798a11b8f5f0ad5549986
Michael Inma <112844696+Divorso@users.noreply.github.com>
2024-03-14 18:44:00 +0700

    clear localstorage
    added clearing of localstorage on quit and leave button. Only clear game and color. (not name)

--- client/JS_modules/checkIfGameIsFull.mjs
+++ client/JS_modules/checkIfGameIsFull.mjs
@@ -1,7 +1,7 @@
 import runGamePage from './runGamePage.mjs';
 
 export default function isGameFull(game) {
-  if (game[0].pictureColors.length === 0) {
+  if (!game[0].pictureColors.length === 0) {
     console.log('game is full');
   } else {
     console.log('game is starting!');
