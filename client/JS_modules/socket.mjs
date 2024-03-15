d8eaf91a373d2e9a7152b1e9f933497365f10300
Amanda Hansson <amanda9307@gmail.com>
2024-03-15 12:09:55 +0100

    Final fix for DO deployment

--- client/JS_modules/socket.mjs
+++ client/JS_modules/socket.mjs
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:3000"); //  https://squid-app-cg7rw.ondigitalocean.app/
+const socket = io("https://starfish-app-b6b63.ondigitalocean.app/");
 
 export { socket }; 
